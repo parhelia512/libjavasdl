e7360dcdd762392b160895153d0fe2e67210ad34
Kamil Ševeček <kamil@sevecek.net>
2021-09-02 14:51:17 +0300

    Fix Checkstyle errors

--- src/main/java/net/mcclendo/libjavasdl/api/haptic/SDL_HapticRamp.java
+++ src/main/java/net/mcclendo/libjavasdl/api/haptic/SDL_HapticRamp.java
@@ -4,13 +4,13 @@ import net.mcclendo.libjavasdl.jna.AbstractSdlStructure;
 
 public final class SDL_HapticRamp extends AbstractSdlStructure {
 
-    public short type;         
+    public short type;
     public SDL_HapticDirection direction;
-    public int length;        
+    public int length;
     public short delay;
-    public short button;        
+    public short button;
     public short interval;
-    public short start;         
+    public short start;
     public short end;
     public short attackLength;
     public short attackLevel;
