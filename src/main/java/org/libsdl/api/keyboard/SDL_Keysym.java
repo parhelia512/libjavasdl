cf8802b819a9661fb2fa09451c6582cc655e7348
Kamil Ševeček <kamil@sevecek.net>
2021-09-05 15:02:16 +0300

    Adapt SDL_keyboard.h

--- src/main/java/org/libsdl/api/keyboard/SDL_Keysym.java
+++ src/main/java/org/libsdl/api/keyboard/SDL_Keysym.java
@@ -1,6 +1,9 @@
 package org.libsdl.api.keyboard;
 
 import com.sun.jna.Structure;
+import org.intellij.lang.annotations.MagicConstant;
+import org.libsdl.api.keycode.SDL_Keycode;
+import org.libsdl.api.scancode.SDL_Scancode;
 
 @Structure.FieldOrder({
         "scancode",
@@ -10,8 +13,13 @@ import com.sun.jna.Structure;
 })
 public final class SDL_Keysym extends Structure {
 
+    @MagicConstant(valuesFromClass = SDL_Scancode.class)
     public int scancode;
+
+    @MagicConstant(valuesFromClass = SDL_Keycode.class)
     public int sym;
+
     public short mod;
+
     public int unused;
 }
