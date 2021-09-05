f3f781aaa8cd55a466daa120674c77f6351038de
Kamil Ševeček <kamil@sevecek.net>
2021-09-05 11:10:59 +0300

    Adapt SDL_quit.h

---
+++ src/main/java/org/libsdl/api/quit/SdlQuit.java
@@ -0,0 +1,18 @@
+package org.libsdl.api.quit;
+
+import static org.libsdl.api.event.SdlEvents.SDL_PEEKEVENT;
+import static org.libsdl.api.event.SdlEvents.SDL_PeepEvents;
+import static org.libsdl.api.event.SdlEvents.SDL_PumpEvents;
+import static org.libsdl.api.event.SdlEvents.SDL_QUIT;
+
+public class SdlQuit {
+
+    private SdlQuit() {
+    }
+
+    public static boolean SDL_QuitRequested() {
+        SDL_PumpEvents();
+        int eventCount = SDL_PeepEvents(null, 0, SDL_PEEKEVENT, SDL_QUIT, SDL_QUIT);
+        return eventCount > 0;
+    }
+}
