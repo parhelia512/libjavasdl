95748139c013dbc4b0cd74e52c7de7386485d461
Kamil Ševeček <kamil@sevecek.net>
2021-09-06 16:31:11 +0300

    Introduce size_t

--- src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
+++ src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
@@ -22,16 +22,6 @@ public final class SdlStdinc {
     public static native int SDL_setenv(String name, String value, int overwrite);
 
 //    // TODO: Test but disable
-//    public static class size_t extends IntegerType {
-//
-//        public size_t() {
-//            this(0);
-//        }
-//
-//        public size_t(long value) {
-//            super(Native.SIZE_T_SIZE, value);
-//        }
-//    }
 //
 //    public static native Pointer SDL_malloc(size_t size);
 //
