97ecef3d607d5405e065d7673de3d8d71c4e5922
Kamil Ševeček <kamil@sevecek.net>
2021-09-09 09:21:59 +0200

    Fix reference in the structure and add docs

--- src/main/java/org/libsdl/api/surface/SDL_Surface.java
+++ src/main/java/org/libsdl/api/surface/SDL_Surface.java
@@ -2,20 +2,65 @@ package org.libsdl.api.surface;
 
 import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
+import org.libsdl.api.pixels.SDL_PixelFormat;
 import org.libsdl.api.rect.SDL_Rect;
 
-public final class SDL_Surface extends Structure {
+/**
+ * <p>A collection of pixels used in software blitting.</p>
+ *
+ * @apiNote This structure should be treated as read-only, except for {@code pixels},
+ * which, if not null, contains the raw pixel data for the surface.
+ */
+@Structure.FieldOrder({
+        "flags",
+        "format",
+        "w",
+        "h",
+        "pitch",
+        "pixels",
+        "userdata",
+        "locked",
+        "lockData",
+        "clipRect",
+        "map",
+        "refcount"
+})
+public class SDL_Surface extends Structure {
 
     public int flags;
-    public Pointer format;
+    public SDL_PixelFormat.ByReference format;
     public int w;
     public int h;
     public int pitch;
     public Pointer pixels;
+
+    /**
+     * Application data associated with the surface
+     */
     public Pointer userdata;
+
+    /**
+     * information needed for surfaces requiring locks
+     */
     public int locked;
+
+    /**
+     * list of BlitMap that hold a reference to this surface
+     */
     public Pointer lockData;
+
+    /**
+     * clipping information
+     */
     public SDL_Rect clipRect;
+
+    /**
+     * info for fast blit mapping to other surfaces
+     */
     public Pointer map;
+
+    /**
+     * Reference count -- used when freeing surface
+     */
     public int refcount;
 }
