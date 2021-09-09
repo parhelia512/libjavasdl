958c30703396a5a46cec37927db3c3e4ea5dcc73
Kamil Ševeček <kamil@sevecek.net>
2021-09-09 14:37:56 +0200

    Add @MagicConstant for better code completion

--- src/main/java/org/libsdl/api/endian/SdlEndianConst.java
+++ src/main/java/org/libsdl/api/endian/SdlEndianConst.java
@@ -1,11 +1,14 @@
 package org.libsdl.api.endian;
 
 import java.nio.ByteOrder;
+import org.intellij.lang.annotations.MagicConstant;
 
 public class SdlEndianConst {
 
     public static final int SDL_LIL_ENDIAN = 1234;
     public static final int SDL_BIG_ENDIAN = 4321;
+
+    @MagicConstant(valuesFromClass = SdlEndianConst.class)
     public static final int SDL_BYTEORDER;
 
     static {
