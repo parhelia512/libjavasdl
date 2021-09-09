8779a9137143f92cf9ad16872a2f1f652d865bae
Kamil Ševeček <kamil@sevecek.net>
2021-09-09 09:48:16 +0200

    Fix Points[] in a native method

--- src/main/java/org/libsdl/api/rect/SDL_Point.java
+++ src/main/java/org/libsdl/api/rect/SDL_Point.java
@@ -1,12 +1,13 @@
 package org.libsdl.api.rect;
 
 import com.sun.jna.Structure;
+import org.libsdl.jna.JnaStructure;
 
 @Structure.FieldOrder({
         "x",
         "y"
 })
-public final class SDL_Point extends Structure {
+public final class SDL_Point extends JnaStructure {
 
     public int x;
     public int y;
