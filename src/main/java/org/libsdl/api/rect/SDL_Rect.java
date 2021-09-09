5dc1f043693d5c41d10f736c79d0e898fe794224
Kamil Ševeček <kamil@sevecek.net>
2021-09-09 09:21:20 +0200

    Expose useMemory() and a constructor with Pointer

--- src/main/java/org/libsdl/api/rect/SDL_Rect.java
+++ src/main/java/org/libsdl/api/rect/SDL_Rect.java
@@ -1,5 +1,6 @@
 package org.libsdl.api.rect;
 
+import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 
 @Structure.FieldOrder({
@@ -14,4 +15,25 @@ public final class SDL_Rect extends Structure {
     public int y;
     public int w;
     public int h;
+
+    public SDL_Rect() {
+    }
+
+    public SDL_Rect(Pointer p) {
+        super(p);
+    }
+
+    /**
+     * Set the memory used by this structure.  This method is used to
+     * indicate the given structure is based on natively-allocated data,
+     * nested within another, or otherwise overlaid on existing memory and
+     * thus does not own its own memory allocation.
+     *
+     * @param m      Base memory to use to back this structure.
+     * @param offset offset into provided memory where structure mapping
+     */
+    @Override
+    public void useMemory(Pointer m, int offset) {
+        super.useMemory(m, offset);
+    }
 }
