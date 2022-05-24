91e139fe299da8e46bc24aed5787297c703e6a7a
Kamil Ševeček <kamil@sevecek.net>
2022-05-24 11:32:30 +0200

    Adapt SDL_vulkan.h

---
+++ src/main/java/org/libsdl/api/vulkan/SdlVulkan.java
@@ -0,0 +1,62 @@
+package org.libsdl.api.vulkan;
+
+import java.util.Arrays;
+import java.util.List;
+import com.sun.jna.Memory;
+import com.sun.jna.Pointer;
+import com.sun.jna.StringArray;
+import com.sun.jna.ptr.ByteByReference;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.PointerByReference;
+import org.libsdl.api.video.SDL_Window;
+import org.libsdl.jna.NativeLoader;
+
+import static org.libsdl.api.error.SdlError.SDL_GetError;
+
+public class SdlVulkan {
+
+    static {
+        NativeLoader.registerNativeMethods(SdlVulkan.class);
+    }
+
+    public static native int SDL_Vulkan_LoadLibrary(
+            String path);
+
+    public static native Pointer SDL_Vulkan_GetVkGetInstanceProcAddr();
+
+    public static native void SDL_Vulkan_UnloadLibrary();
+
+    public static List<String> SDL_Vulkan_GetInstanceExtensions(
+            SDL_Window window) {
+        IntByReference requiredStringArrayLength = new IntByReference(0);
+        if (!SDL_Vulkan_GetInstanceExtensions(window, requiredStringArrayLength, null)) {
+            return null;
+        }
+
+        String[] extensions = new String[requiredStringArrayLength.getValue()];
+        StringArray stringArray = new StringArray(extensions);
+        if (!SDL_Vulkan_GetInstanceExtensions(window, requiredStringArrayLength, stringArray)) {
+            return null;
+        }
+        stringArray.read();
+        return Arrays.asList(extensions);
+    }
+
+    /**
+     * @deprecated Use more Java-style version: {@link #SDL_Vulkan_GetInstanceExtensions(SDL_Window)}
+     */
+    public static native boolean SDL_Vulkan_GetInstanceExtensions(
+            SDL_Window window,
+            IntByReference pCount,
+            StringArray pNames);
+
+    public static native boolean SDL_Vulkan_CreateSurface(
+            SDL_Window window,
+            Pointer instance,
+            PointerByReference surface);
+
+    public static native void SDL_Vulkan_GetDrawableSize(
+            SDL_Window window,
+            IntByReference w,
+            IntByReference h);
+}
