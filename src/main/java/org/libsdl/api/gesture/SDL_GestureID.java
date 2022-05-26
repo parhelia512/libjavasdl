ad184e324e2f5d2041df8cafa84b84ac138b18a3
Kamil Ševeček <kamil@sevecek.net>
2022-05-26 13:46:35 +0200

    Revise SDL_gesture.h to make it more consistent

--- src/main/java/org/libsdl/api/gesture/SDL_GestureID.java
+++ src/main/java/org/libsdl/api/gesture/SDL_GestureID.java
@@ -2,10 +2,10 @@ package org.libsdl.api.gesture;
 
 import com.sun.jna.IntegerType;
 
-public class SDL_GestureID extends IntegerType {
+public final class SDL_GestureID extends IntegerType {
 
     public SDL_GestureID() {
-        super(8, false);
+        this(0L);
     }
 
     public SDL_GestureID(long value) {
--- src/main/java/org/libsdl/api/gesture/SdlGesture.java
+++ src/main/java/org/libsdl/api/gesture/SdlGesture.java
@@ -4,45 +4,26 @@ import org.libsdl.api.rwops.SDL_RWops;
 import org.libsdl.api.touch.SDL_TouchID;
 import org.libsdl.jna.NativeLoader;
 
-public class SdlGesture {
+public final class SdlGesture {
 
-    public static int SDL_RecordGesture(SDL_TouchID touchId) {
-        return NativeFunctions.SDL_RecordGesture(touchId);
+    static {
+        NativeLoader.registerNativeMethods(SdlGesture.class);
     }
 
-    public static int SDL_SaveAllDollarTemplates(SDL_RWops dst) {
-        return NativeFunctions.SDL_SaveAllDollarTemplates(dst);
-    }
+    public static native int SDL_RecordGesture(
+            SDL_TouchID touchId);
 
-    public static int SDL_SaveDollarTemplate(SDL_GestureID gestureId, SDL_RWops dst) {
-        return NativeFunctions.SDL_SaveDollarTemplate(gestureId, dst);
-    }
+    public static native int SDL_SaveAllDollarTemplates(
+            SDL_RWops dst);
 
-    public static int SDL_LoadDollarTemplates(SDL_TouchID touchId, SDL_RWops src) {
-        return NativeFunctions.SDL_LoadDollarTemplates(touchId, src);
-    }
+    public static native int SDL_SaveDollarTemplate(
+            SDL_GestureID gestureId,
+            SDL_RWops dst);
 
-    private static final class NativeFunctions {
+    public static native int SDL_LoadDollarTemplates(
+            SDL_TouchID touchId,
+            SDL_RWops src);
 
-        static {
-            NativeLoader.registerNativeMethods(NativeFunctions.class);
-        }
-
-        private NativeFunctions() {
-        }
-
-        public static native int SDL_RecordGesture(
-                SDL_TouchID touchId);
-
-        public static native int SDL_SaveAllDollarTemplates(
-                SDL_RWops dst);
-
-        public static native int SDL_SaveDollarTemplate(
-                SDL_GestureID gestureId,
-                SDL_RWops dst);
-
-        public static native int SDL_LoadDollarTemplates(
-                SDL_TouchID touchId,
-                SDL_RWops src);
+    private SdlGesture() {
     }
 }
