48d44e8be1d2fb46383e667c68b2c0d2ed13462f
Kamil Ševeček <kamil@sevecek.net>
2022-05-30 14:10:55 +0200

    Revise SDL_power.h to make it more consistent

--- src/main/java/org/libsdl/api/power/SDL_PowerState.java
+++ src/main/java/org/libsdl/api/power/SDL_PowerState.java
@@ -10,6 +10,8 @@ public final class SDL_PowerState implements JnaEnum {
     public static final int SDL_POWERSTATE_CHARGING = 3;
     public static final int SDL_POWERSTATE_CHARGE = 4;
 
+    // TODO: Generate public static String toString(int value)
+
     private SDL_PowerState() {
     }
 }
