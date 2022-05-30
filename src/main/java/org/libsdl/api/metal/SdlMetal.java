9e304dfadc7b170da3cb6ae83bf0a604016bbbe9
Kamil Ševeček <kamil@sevecek.net>
2022-05-30 11:18:54 +0200

    Revise SDL_metal.h to make it more consistent

--- src/main/java/org/libsdl/api/metal/SdlMetal.java
+++ src/main/java/org/libsdl/api/metal/SdlMetal.java
@@ -8,46 +8,30 @@ import org.libsdl.jna.NativeLoader;
 // TODO: Test on macOS
 
 /**
- *  <p>Adapted from SDL_metal.h</p>
+ * <p>Adapted from SDL_metal.h</p>
  *
- *  <p>Functions for creating Metal layers and views on macOS/iOS on top of an SDL window.</p>
+ * <p>Functions for creating Metal layers and views on macOS/iOS on top of an SDL window.</p>
  */
 public final class SdlMetal {
 
-    public static SDL_MetalView SDL_Metal_CreateView(SDL_Window window) {
-        return NativeFunctions.SDL_Metal_CreateView(window);
+    static {
+        NativeLoader.registerNativeMethods(SdlMetal.class);
     }
 
-    public static void SDL_Metal_DestroyView(SDL_MetalView view) {
-        NativeFunctions.SDL_Metal_DestroyView(view);
+    private SdlMetal() {
     }
 
-    public static Pointer SDL_Metal_GetLayer(SDL_MetalView view) {
-        return NativeFunctions.SDL_Metal_GetLayer(view);
-    }
+    public static native SDL_MetalView SDL_Metal_CreateView(
+            SDL_Window window);
 
-    public static void SDL_Metal_GetDrawableSize(SDL_Window window, IntByReference w, IntByReference h) {
-        NativeFunctions.SDL_Metal_GetDrawableSize(window, w, h);
-    }
+    public static native void SDL_Metal_DestroyView(
+            SDL_MetalView view);
 
-    private static final class NativeFunctions {
+    public static native Pointer SDL_Metal_GetLayer(
+            SDL_MetalView view);
 
-        static {
-            NativeLoader.registerNativeMethods(NativeFunctions.class);
-        }
-
-        public static native SDL_MetalView SDL_Metal_CreateView(
-                SDL_Window window);
-
-        public static native void SDL_Metal_DestroyView(
-                SDL_MetalView view);
-
-        public static native Pointer SDL_Metal_GetLayer(
-                SDL_MetalView view);
-
-        public static native void SDL_Metal_GetDrawableSize(
-                SDL_Window window,
-                IntByReference w,
-                IntByReference h);
-    }
+    public static native void SDL_Metal_GetDrawableSize(
+            SDL_Window window,
+            IntByReference w,
+            IntByReference h);
 }
