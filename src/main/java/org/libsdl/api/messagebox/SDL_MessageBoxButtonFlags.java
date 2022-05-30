9c66c10955953ca96130439bf3f829fc3f4bc3fd
Kamil Ševeček <kamil@sevecek.net>
2022-05-30 11:41:03 +0200

    Fix Messagebox toStrings

--- src/main/java/org/libsdl/api/messagebox/SDL_MessageBoxButtonFlags.java
+++ src/main/java/org/libsdl/api/messagebox/SDL_MessageBoxButtonFlags.java
@@ -7,18 +7,25 @@ public final class SDL_MessageBoxButtonFlags implements JnaEnum {
     public static final int SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT = 0x00000001;
     public static final int SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT = 0x00000002;
 
-    public static String toString(int value) {
-        StringBuilder result = new StringBuilder(39*2 + 3);
-        if ((value & SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT) > 0) {
-            result.append("SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT");
+    public static String toString(int type) {
+        StringBuilder result = new StringBuilder(39 * 2 + 3);
+        if ((type & SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT) > 0) {
+            append(result, "SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT");
         }
-        if ((value & SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT) > 0) {
-            if (result.length() > 0) {
-                result.append(" | ");
-            }
-            result.append("SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT");
+        if ((type & SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT) > 0) {
+            append(result, "SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT");
+        }
+        if (result.length() == 0) {
+            result.append("0");
         }
         return result.toString();
+    }
+
+    private static void append(StringBuilder result, String name) {
+        if (result.length() > 0) {
+            result.append(" | ");
+        }
+        result.append(name);
     }
 
     private SDL_MessageBoxButtonFlags() {
--- src/main/java/org/libsdl/api/messagebox/SDL_MessageBoxFlags.java
+++ src/main/java/org/libsdl/api/messagebox/SDL_MessageBoxFlags.java
@@ -10,7 +10,35 @@ public final class SDL_MessageBoxFlags implements JnaEnum {
     public static final int SDL_MESSAGEBOX_BUTTONS_LEFT_TO_RIGHT = 0x00000080;
     public static final int SDL_MESSAGEBOX_BUTTONS_RIGHT_TO_LEFT = 0x00000100;
 
-    // TODO: Generate public static String toString(int value)
+    public static String toString(int type) {
+        StringBuilder result = new StringBuilder(36);
+        if ((type & SDL_MESSAGEBOX_ERROR) > 0) {
+            append(result, "SDL_MESSAGEBOX_ERROR");
+        }
+        if ((type & SDL_MESSAGEBOX_WARNING) > 0) {
+            append(result, "SDL_MESSAGEBOX_WARNING");
+        }
+        if ((type & SDL_MESSAGEBOX_INFORMATION) > 0) {
+            append(result, "SDL_MESSAGEBOX_INFORMATION");
+        }
+        if ((type & SDL_MESSAGEBOX_BUTTONS_LEFT_TO_RIGHT) > 0) {
+            append(result, "SDL_MESSAGEBOX_BUTTONS_LEFT_TO_RIGHT");
+        }
+        if ((type & SDL_MESSAGEBOX_BUTTONS_RIGHT_TO_LEFT) > 0) {
+            append(result, "SDL_MESSAGEBOX_BUTTONS_RIGHT_TO_LEFT");
+        }
+        if (result.length() == 0) {
+            result.append("0");
+        }
+        return result.toString();
+    }
+
+    private static void append(StringBuilder result, String name) {
+        if (result.length() > 0) {
+            result.append(" | ");
+        }
+        result.append(name);
+    }
 
     private SDL_MessageBoxFlags() {
     }
