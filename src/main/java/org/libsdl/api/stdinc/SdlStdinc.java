a5a098f132f2fcc02225dde83e718cce2bfbe761
Kamil Ševeček <kamil@sevecek.net>
2022-06-20 11:23:00 +0200

    Revise SDL_stdinc.h to make it more consistent

--- src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
+++ src/main/java/org/libsdl/api/stdinc/SdlStdinc.java
@@ -2,6 +2,7 @@ package org.libsdl.api.stdinc;
 
 import com.sun.jna.Pointer;
 import org.libsdl.jna.NativeLoader;
+import org.libsdl.jna.size_t;
 
 @SuppressWarnings({
         "checkstyle:MagicNumber",
@@ -21,37 +22,16 @@ public final class SdlStdinc {
             int b,
             int c,
             int d) {
-        return a | (b << 8) | (c << 16) | (d << 24);
+        return (a & 0xFF) | ((b & 0xFF) << 8) | ((c & 0xFF) << 16) | ((d & 0xFF) << 24);
     }
 
-//    public static native String SDL_getenv(String name);
+    public static native Pointer SDL_malloc(size_t size);
 
-//    public static native int SDL_setenv(String name, String value, int overwrite);
+    public static native Pointer SDL_calloc(int nmemb, size_t size);
 
-//    // TODO: Test and then possibly enable
-//    public static native Pointer SDL_malloc(size_t size);
-//
-//    public static native Pointer SDL_calloc(int nmemb, size_t size);
-//
-//    public static native Pointer SDL_realloc(Pointer mem, size_t size);
+    public static native Pointer SDL_realloc(Pointer mem, size_t size);
 
     public static native void SDL_free(Pointer mem);
-
-//    /**
-//     * Get the current set of SDL memory functions
-//     */
-//    public static native void SDL_GetMemoryFunctions(PointerByReference malloc_func,
-//                                                     PointerByReference calloc_func,
-//                                                     PointerByReference realloc_func,
-//                                                     PointerByReference free_func);
-//
-//    /**
-//     * Replace SDL's memory allocation functions with a custom set
-//     */
-//    public static native int SDL_SetMemoryFunctions(Pointer malloc_func,
-//                                                    Pointer calloc_func,
-//                                                    Pointer realloc_func,
-//                                                    Pointer free_func);
 
     /**
      * Get the number of outstanding (unfreed) allocations
---
+++ src/test/java/org/libsdl/api/stdinc/SdlStdincTest.java
@@ -0,0 +1,26 @@
+package org.libsdl.api.stdinc;
+
+import com.sun.jna.Pointer;
+import org.junit.jupiter.api.Test;
+
+import org.libsdl.jna.size_t;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.libsdl.api.stdinc.SdlStdinc.SDL_GetNumAllocations;
+import static org.libsdl.api.stdinc.SdlStdinc.SDL_free;
+import static org.libsdl.api.stdinc.SdlStdinc.SDL_malloc;
+
+public class SdlStdincTest {
+
+    public static final int MAX = 1_000_000;
+
+    @Test
+    public void allocationShouldNotCauseMemoryLeak() {
+        int allocationCount = SDL_GetNumAllocations();
+        for (int i = 0; i < MAX; i++) {
+            Pointer memoryBlob = SDL_malloc(new size_t(65536));
+            SDL_free(memoryBlob);
+        }
+        assertEquals(0, allocationCount);
+    }
+}
