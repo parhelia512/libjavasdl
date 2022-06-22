d3beec92962c838bc6ab11dbc686cb3b648d9098
Kamil Ševeček <kamil@sevecek.net>
2022-06-22 18:04:37 +0200

    Revise SDL_version.h to make it more consistent

--- src/main/java/org/libsdl/api/version/SdlVersion.java
+++ src/main/java/org/libsdl/api/version/SdlVersion.java
@@ -14,12 +14,32 @@ public final class SdlVersion {
     private SdlVersion() {
     }
 
-    public static SDL_version SDL_VERSION() {
-        SDL_version v = new SDL_version();
-        v.major = SdlVersionConst.SDL_MAJOR_VERSION;
-        v.minor = SdlVersionConst.SDL_MINOR_VERSION;
-        v.patch = SdlVersionConst.SDL_PATCHLEVEL;
-        return v;
+    // TODO: Custom documentation
+
+    public static SDL_version SDL_GetLinkedLibraryVersion() {
+        SDL_version ver = new SDL_version();
+        SDL_GetVersion(ver);
+        return ver;
+    }
+
+    public static SDL_version SDL_GetJavaBindingsVersion() {
+        SDL_version ver = new SDL_version();
+        SDL_VERSION(ver);
+        return ver;
+    }
+
+    public static void SDL_EnsureLinkedLibraryMatchesAtLeastJavaBindingsVersion() {
+        SDL_version linkedLibraryVersion = SDL_GetLinkedLibraryVersion();
+        if (!SDL_VERSION_ATLEAST(linkedLibraryVersion.major, linkedLibraryVersion.minor, linkedLibraryVersion.patch)) {
+            throw new IllegalStateException("Minimum version of the SDL library found (" + linkedLibraryVersion + ") is older than the application requires (" + SDL_GetJavaBindingsVersion() + ")");
+        }
+    }
+
+    public static void SDL_VERSION(
+            SDL_version ver) {
+        ver.major = SdlVersionConst.SDL_MAJOR_VERSION;
+        ver.minor = SdlVersionConst.SDL_MINOR_VERSION;
+        ver.patch = SdlVersionConst.SDL_PATCHLEVEL;
     }
 
     public static int SDL_VERSIONNUM(
