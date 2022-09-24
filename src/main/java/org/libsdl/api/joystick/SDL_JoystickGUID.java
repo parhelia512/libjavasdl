ea9fa34dee2d1a7a5631d67692c732e6f21a0183
Kamil Ševeček <kamil@sevecek.net>
2022-09-24 11:56:54 +0200

    Migrate SDL_JoystickGUID on top of SDL_GUID

--- src/main/java/org/libsdl/api/joystick/SDL_JoystickGUID.java
+++ src/main/java/org/libsdl/api/joystick/SDL_JoystickGUID.java
@@ -1,31 +1,18 @@
 package org.libsdl.api.joystick;
 
-import java.util.UUID;
 import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
+import org.libsdl.api.guid.SDL_GUID;
 
 /**
  * A structure that encodes the stable unique id for a joystick device
  */
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
-@Structure.FieldOrder({
-        "leastSigBits",
-        "mostSigBits"
-})
-public final class SDL_JoystickGUID extends Structure implements Structure.ByValue {
-
-    public long leastSigBits;
-    public long mostSigBits;
+public final class SDL_JoystickGUID extends SDL_GUID {
 
     public SDL_JoystickGUID() {
     }
 
     public SDL_JoystickGUID(Pointer p) {
         super(p);
-    }
-
-    @Override
-    public String toString() {
-        return new UUID(mostSigBits, leastSigBits).toString();
     }
 }
