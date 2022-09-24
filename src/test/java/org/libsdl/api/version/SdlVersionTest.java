744e7cc8085e784cbe2d09be2d9bd43abb830b76
Kamil Ševeček <kamil@sevecek.net>
2022-09-24 21:31:42 +0200

    Make SdlVersion more practical

--- src/test/java/org/libsdl/api/version/SdlVersionTest.java
+++ src/test/java/org/libsdl/api/version/SdlVersionTest.java
@@ -2,11 +2,25 @@ package org.libsdl.api.version;
 
 import org.junit.jupiter.api.Test;
 
+import static org.libsdl.api.version.SdlVersion.SDL_CheckLinkedLibraryMatchesAtLeastJavaBindingsVersion;
+import static org.libsdl.api.version.SdlVersion.SDL_GetLinkedLibraryVersion;
+
 public final class SdlVersionTest {
 
     @Test
     public void control() {
-        final SDL_version version = new SDL_version();
-        SdlVersion.SDL_GetVersion(version);
+        SDL_version libVersion = SDL_GetLinkedLibraryVersion();
+        System.out.println("Dynamic C library SDL2 version: " + libVersion);
+
+        SDL_version javaBindingsVersion = SDL_GetLinkedLibraryVersion();
+        System.out.println("Java SDL bindings version: " + javaBindingsVersion);
+
+        System.out.print("Dynamic C library should be of the same or newer version than Java bindings ... ");
+        boolean compatible = SDL_CheckLinkedLibraryMatchesAtLeastJavaBindingsVersion();
+        if (compatible) {
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR");
+        }
     }
 }
