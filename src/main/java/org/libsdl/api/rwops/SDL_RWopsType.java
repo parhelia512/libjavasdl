54b880bfd1112c00b2874c5b8df095234bb3398b
Kamil Ševeček <kamil@sevecek.net>
2022-09-24 11:32:46 +0200

    Fix comment indentation

--- src/main/java/org/libsdl/api/rwops/SDL_RWopsType.java
+++ src/main/java/org/libsdl/api/rwops/SDL_RWopsType.java
@@ -4,40 +4,25 @@ import org.libsdl.jna.JnaEnum;
 
 public final class SDL_RWopsType implements JnaEnum {
 
-    /**
-     * Unknown stream type
-     */
+    /** Unknown stream type */
     public static final int SDL_RWOPS_UNKNOWN = 0;
 
-    /**
-     * Win32 file
-     */
-
+    /** Win32 file */
     public static final int SDL_RWOPS_WINFILE = 1;
 
-    /**
-     * Stdio file
-     */
+    /** Stdio file */
     public static final int SDL_RWOPS_STDFILE = 2;
 
-    /**
-     * Android asset
-     */
+    /** Android asset */
     public static final int SDL_RWOPS_JNIFILE = 3;
 
-    /**
-     * Memory stream
-     */
+    /** Memory stream */
     public static final int SDL_RWOPS_MEMORY = 4;
 
-    /**
-     * Read-Only memory stream
-     */
+    /** Read-Only memory stream */
     public static final int SDL_RWOPS_MEMORY_RO = 5;
 
-    /**
-     * Vita file
-     */
+    /** Vita file */
     public static final int SDL_RWOPS_VITAFILE = 6;
 
     // TODO: Generate public static String toString(int value)
