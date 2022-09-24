3621b5f6becce42f36bd15160d0fe33a8903d74f
Kamil Ševeček <kamil@sevecek.net>
2022-09-24 11:55:07 +0200

    Upgrade SdlGameController

--- src/main/java/org/libsdl/api/gamecontroller/SDL_GameControllerType.java
+++ src/main/java/org/libsdl/api/gamecontroller/SDL_GameControllerType.java
@@ -14,6 +14,10 @@ public final class SDL_GameControllerType implements JnaEnum {
     public static final int SDL_CONTROLLER_TYPE_PS5 = 7;
     public static final int SDL_CONTROLLER_TYPE_AMAZON_LUNA = 8;
     public static final int SDL_CONTROLLER_TYPE_GOOGLE_STADIA = 9;
+    public static final int SDL_CONTROLLER_TYPE_NVIDIA_SHIELD = 10;
+    public static final int SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_JOYCON_LEFT = 11;
+    public static final int SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_JOYCON_RIGHT = 12;
+    public static final int SDL_CONTROLLER_TYPE_NINTENDO_SWITCH_JOYCON_PAIR = 13;
 
     // TODO: Generate public static String toString(int value)
 
