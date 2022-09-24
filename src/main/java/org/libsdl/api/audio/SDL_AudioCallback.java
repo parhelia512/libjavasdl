3444ebe49c2475e502d48925504b59a5104e50a3
Kamil Ševeček <kamil@sevecek.net>
2022-09-24 21:50:16 +0200

    Reformat Javadoc

--- src/main/java/org/libsdl/api/audio/SDL_AudioCallback.java
+++ src/main/java/org/libsdl/api/audio/SDL_AudioCallback.java
@@ -7,18 +7,18 @@ import com.sun.jna.Pointer;
 public interface SDL_AudioCallback extends Callback {
 
     /**
-     *  This function is called when the audio device needs more data.
+     * This function is called when the audio device needs more data.
      *
-     *  <p>Once the callback returns, the buffer will no longer be valid.
-     *  Stereo samples are stored in a LRLRLR ordering.</p>
+     * <p>Once the callback returns, the buffer will no longer be valid.
+     * Stereo samples are stored in a LRLRLR ordering.</p>
      *
-     *  <p>You can choose to avoid callbacks and use SDL_QueueAudio() instead, if
-     *  you like. Just open your audio device with a {@code null} callback.</p>
+     * <p>You can choose to avoid callbacks and use SDL_QueueAudio() instead, if
+     * you like. Just open your audio device with a {@code null} callback.</p>
      *
-     *  @param userdata An application-specific parameter saved in
-     *                  the SDL_AudioSpec structure
-     *  @param stream A pointer to the audio data buffer.
-     *  @param len    The length of that buffer in bytes.
+     * @param userdata An application-specific parameter saved in
+     *                 the SDL_AudioSpec structure
+     * @param stream   A pointer to the audio data buffer.
+     * @param len      The length of that buffer in bytes.
      */
     void callback(
             Pointer userdata,
