36d684eda306e583bb2d60d8538a822beedd93fd
Kamil Ševeček <kamil@sevecek.net>
2022-10-04 12:27:00 +0200

    Add SdlBits documentation

--- src/main/java/org/libsdl/api/bits/SdlBits.java
+++ src/main/java/org/libsdl/api/bits/SdlBits.java
@@ -1,5 +1,10 @@
 package org.libsdl.api.bits;
 
+/**
+ *  Definitions from file SDL_bits.h
+ *
+ *  <p>Functions for fiddling with bits and bitmasks.</p>
+ */
 public final class SdlBits {
 
     private static final int[] BITMASKS = {0b10, 0b1100, 0b11110000, 0xFF00, 0xFFFF0000};
@@ -8,6 +13,13 @@ public final class SdlBits {
     private SdlBits() {
     }
 
+    /**
+     *  Get the index of the most significant bit.
+     *  This operation can also be stated as "count leading zeroes" and
+     *  "log base 2".
+     *
+     *  @return the index of the most significant bit, or -1 if the value is 0.
+     */
     public static int SDL_MostSignificantBitIndex32(
             int x) {
         if (x == 0) {
