e5915e8f7a12ac4c4f21049a2b32be298f112fa6
Kamil Ševeček <kamil@sevecek.net>
2022-10-04 11:25:44 +0200

    Add warning to all callbacks about GC

--- src/main/java/org/libsdl/api/video/SDL_HitTest.java
+++ src/main/java/org/libsdl/api/video/SDL_HitTest.java
@@ -5,6 +5,13 @@ import com.sun.jna.Pointer;
 import org.intellij.lang.annotations.MagicConstant;
 import org.libsdl.api.rect.SDL_Point;
 
+/**
+ * <p><b>Warning:</b> It is necessary to keep a reference to the callback object somewhere in your Java program,
+ * otherwise JNA would dispose of the object (GC would clean it) and the callback function would no longer
+ * be available for SDL library's C code to call.<br/>
+ * In case you did not keep the reference you would encounter an error like this:<br/>
+ * <code>JNA: callback object has been garbage collected</code></p>
+ */
 @FunctionalInterface
 public interface SDL_HitTest extends Callback {
 
