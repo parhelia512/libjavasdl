b4692303128a449aacabb6a6ece273fc21b4a232
Kamil Ševeček <kamil@sevecek.net>
2022-10-07 13:12:38 +0200

    Add SdlLocale documentation

--- src/main/java/org/libsdl/api/locale/SDL_Locale.java
+++ src/main/java/org/libsdl/api/locale/SDL_Locale.java
@@ -9,7 +9,10 @@ import com.sun.jna.Structure;
 })
 public final class SDL_Locale extends Structure {
 
+    /** A language name, like "en" for English. */
     public String language;
+
+    /** A country, like "US" for America. Can be null. */
     public String country;
 
     public SDL_Locale() {
