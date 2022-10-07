a6f44f58b401911a2f2a995419f899269409b8bd
Kamil Ševeček <kamil@sevecek.net>
2022-10-07 18:07:10 +0200

    Add SdlPower documentation

--- src/main/java/org/libsdl/api/power/SDL_PowerState.java
+++ src/main/java/org/libsdl/api/power/SDL_PowerState.java
@@ -2,12 +2,24 @@ package org.libsdl.api.power;
 
 import org.libsdl.jna.JnaEnum;
 
+/**
+ * The basic state for the system's power supply.
+ */
 public final class SDL_PowerState implements JnaEnum {
 
+    /** cannot determine power status */
     public static final int SDL_POWERSTATE_UNKNOWN = 0;
+
+    /** Not plugged in, running on the battery */
     public static final int SDL_POWERSTATE_ON_BATTERY = 1;
+
+    /** Plugged in, no battery available */
     public static final int SDL_POWERSTATE_NO_BATTERY = 2;
+
+    /** Plugged in, charging battery */
     public static final int SDL_POWERSTATE_CHARGING = 3;
+
+    /** Plugged in, battery charged */
     public static final int SDL_POWERSTATE_CHARGE = 4;
 
     // TODO: Generate public static String toString(int value)
