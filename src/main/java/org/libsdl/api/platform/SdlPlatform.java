7eaf3338dfca16c229b87167a2966eed0474f12e
Kamil Ševeček <kamil@sevecek.net>
2022-10-07 18:01:58 +0200

    Add SdlPlatform documentation

--- src/main/java/org/libsdl/api/platform/SdlPlatform.java
+++ src/main/java/org/libsdl/api/platform/SdlPlatform.java
@@ -2,6 +2,11 @@ package org.libsdl.api.platform;
 
 import org.libsdl.jna.SdlNativeLibraryLoader;
 
+/**
+ * Definitions from file SDL_platform.h
+ *
+ * <p>Try to get a standard set of platform defines.</p>
+ */
 public final class SdlPlatform {
 
     static {
@@ -11,5 +16,21 @@ public final class SdlPlatform {
     private SdlPlatform() {
     }
 
+    /**
+     * Get the name of the platform.
+     *
+     * <p>Here are the names returned for some (but not all) supported platforms:</p>
+     * <ul>
+     *     <li>"Windows"</li>
+     *     <li>"Mac OS X"</li>
+     *     <li>"Linux"</li>
+     *     <li>"iOS"</li>
+     *     <li>"Android"</li>
+     * </ul>
+     *
+     * @return the name of the platform. If the correct platform name is not
+     * available, returns a string beginning with the text "Unknown".
+     * @since This function is available since SDL 2.0.0.
+     */
     public static native String SDL_GetPlatform();
 }
